d0e353c6d7d3977a061ec65877fc00a56d7806e1
CuriousNom <aryanbiswas7585@gmail.com>
2026-03-12 19:24:31 +0000

    Revert changes
    This reverts commit 4d2489db50b8b26b91287a5fdfb769d5f00376fb.

--- android/variable.go
+++ android/variable.go
@@ -433,8 +433,6 @@ type ProductVariables struct {
 
 	DeviceKernelHeaders []string `json:",omitempty"`
 
-	TargetSpecificHeaderPath *string `json:",omitempty"`
-
 	ExtraVndkVersions []string `json:",omitempty"`
 
 	NamespacesToExport []string `json:",omitempty"`
--- cc/cc.go
+++ cc/cc.go
@@ -2937,26 +2937,6 @@ func (c *Module) DepsMutator(actx android.BottomUpMutatorContext) {
 	variantNdkLibs := []string{}
 	variantLateNdkLibs := []string{}
 	if ctx.Os() == android.Android {
-		rewriteHeaderLibs := func(list []string) (newHeaderLibs []string) {
-			newHeaderLibs = []string{}
-			for _, entry := range list {
-				// Replace device_kernel_headers with generated_kernel_headers
-				// for inline kernel building
-				if entry == "device_kernel_headers" || entry == "qti_kernel_headers" || entry == "generated_kernel_headers" {
-					if (len(ctx.Config().Getenv("TARGET_PREBUILT_KERNEL_HEADERS")) > 0) {
-						newHeaderLibs = append(newHeaderLibs, "prebuilt_kernel_headers")
-						continue
-					} else if (ctx.Config().Getenv("INLINE_KERNEL_BUILDING") == "true" && entry != "generated_kernel_headers") {
-						newHeaderLibs = append(newHeaderLibs, "generated_kernel_headers")
-						continue
-					}
-				}
-				newHeaderLibs = append(newHeaderLibs, entry)
-			}
-			return newHeaderLibs
-		}
-		deps.HeaderLibs = rewriteHeaderLibs(deps.HeaderLibs)
-
 		deps.SharedLibs, variantNdkLibs = FilterNdkLibs(c, ctx.Config(), deps.SharedLibs)
 		deps.LateSharedLibs, variantLateNdkLibs = FilterNdkLibs(c, ctx.Config(), deps.LateSharedLibs)
 		deps.ReexportSharedLibHeaders, _ = FilterNdkLibs(c, ctx.Config(), deps.ReexportSharedLibHeaders)
